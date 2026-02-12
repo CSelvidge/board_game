5afd1d97acb935dfd2880fabdedea884ca44d7c0
CSelvidge <healincheese@gmail.com>
2026-02-12 01:54:45 -0500

    Early shit

--- cards.c
+++ cards.c
@@ -3,3 +3,20 @@
            Create sets of cards for each destination zone, and gandalf cards 
            Do not forget boromir         
 */
+
+
+typedef struct {
+    char* text;
+    char* card_name;
+    char* owned_by; //Which player is the friend card assigned to
+    unsigned char alignment; // Friend, Foe,  or Special mordor card
+    unsigned char displayed; // Card flipped or not
+    unsigned char discarded; // Card still in play or not
+    unsigned char is_combat; // Does the card invoke combat
+    unsigned char total_combats; // If it does how many
+}Encounter_card;
+
+typedef struct {
+    char* text;
+    unsigned char discarded; // Card still in play or not
+}Gandalf_card;
--- dice.c
+++ dice.c
@@ -4,3 +4,38 @@
            Encounter number dice
            Combat die (singular)
 */
+
+typedef struct {
+    //uint32_t die_color; // Not used in initial REPL version, but once we move to art assets this can be used to color the die
+    char* name;
+    unsigned char sides[6];
+} Fellowship_die;
+
+typedef struct {
+    char *names[6];
+}Combat_die;
+
+typedef struct {
+    unsigned char sides[6];
+}Encounter_die;
+
+Encounter_die init_encounter_die() {
+    Encounter_die die  = {{1, 2, 3, 4, 5, 6}};
+    return die;
+}
+
+Combat_die init_combat_die(char* character_names) {
+    Combat_die die;
+    for ( char i = 0; i < 6; i++) {
+        die.names[i] = character_names[i];
+    }
+    return die;
+}
+
+Fellowship_die init_fellowship_die(char* character_name) {
+    Fellowship_die die = {
+    .name = character_name,
+    .sides = {1, 2, 3, 4, 5, 6}
+    };
+    return die;
+}
